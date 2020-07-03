5e8e9ffe7109fae0503ef2f7c59c69907370875e
maxcramer <maxcramer@MacBook-Pro.local>
2020-07-03 12:51:39 +0100

    adding material UI for the inputs

--- src/components/CheckoutDetails/CheckoutDetails.jsx
+++ src/components/CheckoutDetails/CheckoutDetails.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import TextField from '@material-ui/core/TextField';
+
 import './CheckoutDetails.css';
 
 class CheckoutDetails extends React.Component {
@@ -10,12 +12,12 @@ class CheckoutDetails extends React.Component {
         return (
             <div className="checkout__details">
                 <h2 id="checkout__title">Order yours today!</h2>
-                <input type="text" placeholder="Name"/>
-                <input type="text" placeholder="Email Address"/>
-                <input type="text" placeholder="Name On Card"/>
-                <input type="number" placeholder="Card Number"/>
-                <input type="month" placeholder="MM/YYYY"/>
-                <input type="number" placeholder="CVV"/>
+                <TextField id="standard-basic" type="text" label="Name" />
+                <TextField id="standard-basic" type="email" label="Email Address" />
+                <TextField id="standard-basic" type="text" label="Name On Card" />
+                <TextField id="standard-basic" type="number" label="Card Number" />
+                <TextField id="standard-basic" type="number" label="Card Number" />
+                <TextField id="standard-basic" type="number" label="CVV" />
                 <button>Purchase Now</button>
             </div>
         )
