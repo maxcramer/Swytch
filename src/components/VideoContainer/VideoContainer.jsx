0294c9ecfa21cb5a21e69aa226f14d77f4ad7f55
maxcramer <maxcramer@MacBook-Pro.local>
2020-07-03 15:33:29 +0100

    changing video layout desktop and giving z index to discount and navbar

--- src/components/VideoContainer/VideoContainer.jsx
+++ src/components/VideoContainer/VideoContainer.jsx
@@ -4,10 +4,16 @@ import './VideoContainer.css';
 
 
 function VideoContainer() {
+    let vid1 = document.getElementById('video_1');
+    let vid2 = document.getElementById('video_2');
+    let vidContainer = document.getElementsByClassName('video__container');
+    console.log(vidContainer)
+    
+
     return (
         <div className="video__container">
-                <iframe className="video__big" width="560" height="315" src="https://www.youtube.com/embed/KEgxcv-NAzY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <iframe className="video__small" width="560" height="315" src="https://www.youtube.com/embed/RGCNW2KOtIE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe className="video__sizing" width="560" height="315" src="https://www.youtube.com/embed/KEgxcv-NAzY" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <iframe className="video__sizing" width="560" height="315" src="https://www.youtube.com/embed/RGCNW2KOtIE" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
     )
 }
