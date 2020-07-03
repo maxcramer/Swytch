5d0ca504d504aca6afac9c3bae48f1ec42450125
maxcramer <maxcramer@MacBook-Pro.local>
2020-07-03 18:39:32 +0100

    footer styled for mobile

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,11 +8,11 @@ function Footer () {
         <div className="footer__container">
             <div className="about-links">
                 <div className="about">
-                    <h2>About</h2>
+                    <h2 id="about-title">About</h2>
                     <p>Ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea totam quaerat accusantium corporis eaque velit officia soluta voluptatum at? Illo fugiat nisi exercitationem commodi molestiae distinctio, sit sint ea dolor?</p>
                 </div>
                 <div className="quick__links">
-                    <h2>Quick Links</h2>
+                    <h2 id="quick__links-title">Quick Links</h2>
                     <ul className="quick__links-list">
                         <li><a href="">Menu Item 1</a></li>
                         <li><a href="">Menu Item 2</a></li>
@@ -21,8 +21,8 @@ function Footer () {
                     </ul>
                 </div>
             </div>
-            <div>
-                Copy Right
+            <div className="copyright">
+                <p>© 2020 Company Name. All Rights Reserved.</p>
             </div>
         </div>
     )
