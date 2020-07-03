f7101ef1f4c428c8226d5c066f2458b2093931d5
maxcramer <maxcramer@MacBook-Pro.local>
2020-07-03 15:41:01 +0100

    cleaning up video container jsx file

--- src/components/VideoContainer/VideoContainer.jsx
+++ src/components/VideoContainer/VideoContainer.jsx
@@ -1,15 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './VideoContainer.css';
 
 
 function VideoContainer() {
-    let vid1 = document.getElementById('video_1');
-    let vid2 = document.getElementById('video_2');
-    let vidContainer = document.getElementsByClassName('video__container');
-    console.log(vidContainer)
-    
-
     return (
         <div className="video__container">
                 <iframe className="video__sizing" width="560" height="315" src="https://www.youtube.com/embed/KEgxcv-NAzY" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
