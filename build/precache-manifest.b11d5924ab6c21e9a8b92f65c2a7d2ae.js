138367185ed3f42a4f06f27327cff19ff7618c71
maxcramer <maxcramer@MacBook-Pro.local>
2020-07-03 19:12:56 +0100

    fixing build

---
+++ build/precache-manifest.b11d5924ab6c21e9a8b92f65c2a7d2ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb1f02ac4eeceeb76af06b7f9f229e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4061986dfbbdc0b2576d",
+    "url": "/static/css/main.77e6711f.chunk.css"
+  },
+  {
+    "revision": "88ea3e85d67dc8af2f0d",
+    "url": "/static/js/2.04e2b0a7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.04e2b0a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4061986dfbbdc0b2576d",
+    "url": "/static/js/main.3a7af68f.chunk.js"
+  },
+  {
+    "revision": "5a1109ffadfc3ce88f0e",
+    "url": "/static/js/runtime-main.da91bbca.js"
+  }
+]);
