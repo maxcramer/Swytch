4d4e2b120410dfcd95fe87bbe08b1d768baae8c6
maxcramer <maxcramer@MacBook-Pro.local>
2020-07-03 16:39:10 +0100

    changing Notifications from class based component to notifications component

--- src/components/Notifications/Notifications.jsx
+++ src/components/Notifications/Notifications.jsx
@@ -1,25 +1,17 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-class Notifications extends React.Component{
-    state = {
-        message1: 'Congratulations! XYZ has just purchased a new Swyft Pack',
-        message2: 'Only 11 Swyft Packs Left! Get yours before they all pedal off!',
-        message3: '17 other people are currently looking at this item',
-        message4: 'Only 24 Hours left of the 50% Sale!'
+function Notifications () {
+    const [messages, setMessages] = useState()
+    
+    useEffect(() => {
+        setInterval(() => {
+            console.log("Hello every 3 seconds");
+        }, 3000)
+    })
 
-    }
-
-    render() {
-        return(
-            <div>
-                <p>{this.state.message1}</p>
-                <p>{this.state.message2}</p>
-                <p>{this.state.message3}</p>
-                <p>{this.state.message4}</p>
-
-            </div>
-        )
-    }
+    return (
+        <p>notifications</p>
+    )
 }
 
 export default Notifications;
