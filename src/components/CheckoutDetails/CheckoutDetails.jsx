dec7f6553eee7223810b34732078b4a15aa387c6
maxcramer <maxcramer@MacBook-Pro.local>
2020-07-03 13:43:21 +0100

    checkout details changing to order confirmed text on button click

--- src/components/CheckoutDetails/CheckoutDetails.jsx
+++ src/components/CheckoutDetails/CheckoutDetails.jsx
@@ -6,24 +6,44 @@ import Button from '@material-ui/core/Button';
 import './CheckoutDetails.css';
 
 class CheckoutDetails extends React.Component {
+    state = {
+        orderConfirmed: false
+    }
 
-
+    showConfirmedOrder = () => {
+        this.setState((prevState) => {
+            return {orderConfirmed: !prevState.orderConfirmed}
+        })
+    }
 
     render() {
-        return (
-            <div className="checkout__details">
-                <h2 id="checkout__title">Order yours today!</h2>
-                <TextField id="standard-basic" type="text" label="Name" />
-                <TextField id="standard-basic" type="email" label="Email Address" />
-                <TextField id="standard-basic" type="text" label="Name On Card" />
-                <TextField id="standard-basic" type="number" label="Card Number" />
-                <TextField id="standard-basic" type="number" label="Card Expiration (MM.YY)" />
-                <TextField id="standard-basic" type="number" label="CVV" />
-                <Button variant="contained" color="primary">
-                    Buy Now!
-                </Button>
-            </div>
-        )
+        if(this.state.orderConfirmed) {
+            return (
+                <div>
+                    <h3>Order Confirmed</h3>
+                </div>
+            )
+        } else {
+            return (
+                <div className="checkout__details">
+                    <h2 id="checkout__title">Order yours today!</h2>
+                    <TextField id="standard-basic" type="text" label="Name" />
+                    <TextField id="standard-basic" type="email" label="Email Address" />
+                    <TextField id="standard-basic" type="text" label="Name On Card" />
+                    <TextField id="standard-basic" type="number" label="Card Number" />
+                    <TextField id="standard-basic" type="number" label="Card Expiration (MM.YY)" />
+                    <TextField id="standard-basic" type="number" label="CVV" />
+                    <div >
+                        <Button id="button" variant="contained" color="primary" onClick={this.showConfirmedOrder}>
+                            Buy Now!
+                        </Button>
+                    </div>
+                   
+                </div>
+            )
+        }
+
+        
     }
 }
 
